d0e9774f42339aca390a990c54827dbbd208bad8
Richard Cheng <richardcheng2001@gmail.com>
2017-12-26 23:49:23 -0600

    first commit

---
+++ shoelace.py
@@ -0,0 +1,16 @@
+print("Welcome!")
+
+coordinates = input("Input list for coordinates.")
+
+def shoelace(x):
+    n = len(coordinates)
+    for i <= n:
+        j = 
+        area1 = corners[i][0] * corners[j][1]
+        area2 = corners[j][0] * corners[i][1]
+
+
+area = abs(area1 - area2) / 2.0
+return area
+print(area)
+
